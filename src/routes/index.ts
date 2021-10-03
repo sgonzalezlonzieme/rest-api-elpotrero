7c7558f73373e13519ec3fd0e4554d3c6ba0f0cc
cmanzano91 <catumanzano@gmail.com>
2021-10-03 09:02:30 -0300

    update route index

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import postUser from '../controllers/user/user';
+import fieldRoute from './fields'
 const router = Router();
 
 router.use('/user', postUser)
-
+router.use('/fields', fieldRoute)
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
