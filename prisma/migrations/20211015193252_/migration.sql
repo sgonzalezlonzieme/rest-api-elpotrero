2d758ebf80fd81af9d4a3a05092eef80405bf5fb
sgonzalezlonzieme <sgonzalezlonzieme@gmail.com>
2021-10-15 16:39:24 -0300

    google

--- prisma/migrations/20211015193252_/migration.sql
+++ prisma/migrations/20211015193252_/migration.sql
@@ -4,6 +4,7 @@
   - You are about to drop the column `teamId` on the `Player` table. All the data in the column will be lost.
   - You are about to drop the column `gameId` on the `TimeTable` table. All the data in the column will be lost.
   - You are about to drop the `Game` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `UserGoogle` table. If the table is not empty, all the data it contains will be lost.
   - Added the required column `userId` to the `Team` table without a default value. This is not possible if the table is not empty.
 
 */
@@ -33,23 +34,26 @@ ALTER TABLE "TimeTable" DROP COLUMN "gameId";
 -- DropTable
 DROP TABLE "Game";
 
+-- DropTable
+DROP TABLE "UserGoogle";
+
 -- CreateTable
-CREATE TABLE "_Teams_Players" (
+CREATE TABLE "_Players_Teams" (
     "A" INTEGER NOT NULL,
     "B" INTEGER NOT NULL
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "_Teams_Players_AB_unique" ON "_Teams_Players"("A", "B");
+CREATE UNIQUE INDEX "_Players_Teams_AB_unique" ON "_Players_Teams"("A", "B");
 
 -- CreateIndex
-CREATE INDEX "_Teams_Players_B_index" ON "_Teams_Players"("B");
+CREATE INDEX "_Players_Teams_B_index" ON "_Players_Teams"("B");
 
 -- AddForeignKey
 ALTER TABLE "Team" ADD CONSTRAINT "Team_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "_Teams_Players" ADD FOREIGN KEY ("A") REFERENCES "Player"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "_Players_Teams" ADD FOREIGN KEY ("A") REFERENCES "Player"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "_Teams_Players" ADD FOREIGN KEY ("B") REFERENCES "Team"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "_Players_Teams" ADD FOREIGN KEY ("B") REFERENCES "Team"("id") ON DELETE CASCADE ON UPDATE CASCADE;
